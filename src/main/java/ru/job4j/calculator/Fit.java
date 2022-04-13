0231f142034ddbdab38d07cd1feaaffd3e9c48bc
Defuckto <102257442+Defuckto@users.noreply.github.com>
2022-04-14 00:43:33 +0300

    Update Fit.java

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,17 +1,14 @@
 package ru.job4j.calculator;
 
 public class Fit {
-
     public static double manWeight(short height) {
         double rsl = (height - 100) * 1.15;
         return rsl;
     }
-
     public static double womanWeight(short height) {
         double rsl = (height - 110) * 1.15;
         return rsl;
     }
-
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
@@ -19,5 +16,4 @@ public class Fit {
         System.out.println("Man 187 is " + man);
         System.out.println("Woman 178 is " + woman);
     }
-
 }
